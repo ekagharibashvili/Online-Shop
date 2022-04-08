c4636122a41bfd7a2cc82c8381a0d088ce2a8a1d
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-04-08 16:23:05 +0400

    add ejs templates

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs.engine({
-    extname: "hbs",
-    defaultLayout: 'main-layout',
-    layoutsDir: 'views/layouts/'
-  })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
